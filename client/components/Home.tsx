c6ee4996bed2810a9b2fd660affb8f4ec7421304
Gabriel Rawcliffe <gabriel.rawcliffe@gmail.com>
2023-08-18 10:36:32 +1200

    popup add works

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,10 +1,20 @@
-// ?Propaganda about the Empire, image 
+// ?Propaganda about the Empire, image
+import Popup from './Popup'
+import { useState, useEffect } from 'react'
 
-export default function Home()
-{
+export default function Home() {
+  const [showAd, setShowAd] = useState(false)
+
+  const handleOpenAd = () => {
+    setShowAd(true)
+  }
   return (
     <div>
       This is home page
+      <button className="open-button" onClick={handleOpenAd}>
+        Open Ad
+      </button>
+      <Popup showAd={showAd} />
     </div>
   )
 }
