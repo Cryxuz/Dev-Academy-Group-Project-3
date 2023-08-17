94214b215ade073d0e93ed6b35a299a2c0fb0c99
liang-wang-mako <lwang@micro-wang.com>
2023-08-17 16:49:21 +1200

    Set up the project structure

---
+++ client/router.tsx
@@ -0,0 +1,23 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
+
+import App from './components/App'
+import Home from './components/Home'
+import OurPeople from './components/OurPeople'
+import SignUp from './components/SignUp'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="OurPeople" element={<OurPeople />} />
+      <Route path="SignUp" element={<SignUp />} />
+    </Route>
+  )
+)
+
+export default router
