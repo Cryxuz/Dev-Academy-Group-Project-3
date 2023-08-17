ac5a9f6cda9730228adf55692584d4c818514545
yind48 <dylanyin48@gmail.com>
2023-08-18 10:48:35 +1200

    html-to-react

--- client/components/Signup.tsx
+++ client/components/Signup.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Signup() {
   return (
-    <section className="bg-gray-900 dark:bg-gray-900">
+    <section className="bg-gray-50 dark:bg-gray-900">
       <br />
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <Link
