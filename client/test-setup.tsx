6d352918dee84d656e5037ee60d0e176fd27396d
liang-wang-mako <lwang@micro-wang.com>
2023-08-18 14:15:11 +1200

    Set up test and set empire/people and signup form

---
+++ client/test-setup.tsx
@@ -0,0 +1,22 @@
+import { beforeEach, expect, test } from 'vitest'
+import { cleanup, render } from '@testing-library/react'
+import matchers from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom/vitest'
+
+import { createMemoryRouter, RouterProvider } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
+import { routes } from './router'
+
+beforeEach(cleanup)
+expect.extend(matchers)
+
+export function renderApp(location: string) {
+  const user = userEvent.setup()
+  const router = createMemoryRouter(routes, {
+    initialEntries: [location],
+  })
+
+  const container = render(<RouterProvider router={router} />)
+  return { user, ...container }
+}
+
